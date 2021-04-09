4dad9c8cc5966bdb22108d30f85f05ce5f84d639
Ajibade Olufemi <ajibadeolufemi94@gmail.com>
2021-04-09 23:43:58 +0100

    feat:responsivenes fixed

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const menuIcon = document.querySelector('.hamburger-menu');
-const link = document.querySelector('.nav-bar');
+// const menuIcon = document.querySelector('.hamburger-menu');
+// const link = document.querySelector('.nav-bar');
 
-menuIcon.addEventListener('click', () => {
-    link.classList.toggle("change");
-});  
+// menuIcon.addEventListener('click', () => {
+//     link.classList.toggle("change");
+// });
+
+
+const hamburger = document.querySelector('.hamburger-menu');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links li');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+})
